fcded0f6be8569396510a94c05541ab43135e5d6
user32dll <96720131+user32dll@users.noreply.github.com>
2023-10-02 09:10:32 +0200

    Add keybind
    Adds a keybind to toggle the light level indicators

--- src/client/java/com/jellied/lightlevelindicator/LightLevelIndicatorModClient.java
+++ src/client/java/com/jellied/lightlevelindicator/LightLevelIndicatorModClient.java
@@ -2,13 +2,32 @@ package com.jellied.lightlevelindicator;
 
 import com.fox2code.foxloader.loader.ClientMod;
 import com.fox2code.foxloader.registry.CommandCompat;
+import net.minecraft.src.client.KeyBinding;
+import com.fox2code.foxloader.client.KeyBindingAPI;
+import net.minecraft.client.Minecraft;
+import org.lwjgl.input.Keyboard;
 
 public class LightLevelIndicatorModClient extends LightLevelIndicatorMod implements ClientMod {
+    private static final KeyBinding toggleLightLevelIndicatorKeyBinding = new KeyBinding("Toggle Light Indicators", Keyboard.KEY_L);
+    private static boolean firstPress = true;
+
     public void onTick() {
+        handleKeybind();
         LightLevelIndicatorHandler.update();
     }
 
     public void onInit() {
+        KeyBindingAPI.registerKeyBinding(toggleLightLevelIndicatorKeyBinding);
         CommandCompat.registerClientCommand(new ToggleCommand("toggleLightIndicators"));
+    }
+
+    private void handleKeybind() {
+        if (Keyboard.isKeyDown(toggleLightLevelIndicatorKeyBinding.keyCode) && firstPress) {
+            LightLevelIndicatorHandler.toggleIsEnabled();
+            Minecraft.getInstance().thePlayer.displayChatMessage("Light Level Indicators: " + (LightLevelIndicatorHandler.getIsEnabled() ? "Enabled" : "Disabled"));
+            firstPress = false;
+        } else if (!Keyboard.isKeyDown(toggleLightLevelIndicatorKeyBinding.keyCode)) {
+            firstPress = true;
+        }
     }
 }
